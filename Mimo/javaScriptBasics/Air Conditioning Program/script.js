cd3fc070d345dfdd5f94f7aa5b437314433192f8
VL <lusakvm@gmail.com>
2023-08-20 01:58:45 +0300

    JsBasics

---
+++ Mimo/javaScriptBasics/Air Conditioning Program/script.js
@@ -0,0 +1,7 @@
+const isWeekday = !false;
+let airConditioning = !true;
+let highTemperature = true;
+console.log("Weekday");
+console.log(isWeekday);
+console.log("Air conditioning");
+console.log(airConditioning);
