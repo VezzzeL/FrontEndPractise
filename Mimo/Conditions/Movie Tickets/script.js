07846e882f489d1cd0cbac8faf915db86dde3ccb
VL <lusakvm@gmail.com>
2023-08-29 00:12:52 +0300

    conditions tasks

---
+++ Mimo/Conditions/Movie Tickets/script.js
@@ -0,0 +1,16 @@
+let member_number;
+let age = 27;
+let price;
+const isMember = member_number > 0;
+if (age >= 65) {
+  price = 5;
+} else if (age <= 10) {
+  price = 8;
+} else {
+  price = 10;
+}
+if (isMember) {
+  console.log("Welcome Back!");
+  price = price * 0.8;
+}
+console.log("Ticket Price: $" + price);
