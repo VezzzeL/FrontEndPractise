6910765575d0774906a13a3b0cfbbeb3a35a4e1c
VL <lusakvm@gmail.com>
2023-09-15 23:16:38 +0300

    SQL subqueries task

---
+++ Mimo/SQL Subqueries/Favorite Singers/script.sql
@@ -0,0 +1,18 @@
+SELECT
+    DISTINCT singer
+FROM
+    songs
+WHERE
+    id IN (
+        SELECT
+            song_id
+        FROM
+            saved_playlist
+        WHERE
+            times_played > (
+                SELECT
+                    AVG(times_played)
+                FROM
+                    saved_playlist
+            )
+    )
